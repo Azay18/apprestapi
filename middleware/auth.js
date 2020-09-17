139ee8f282fb2c091453ad70c5abb4cc3e2819df
Azay18 <muhammadrizqiaz@gmail.com>
2020-09-17 19:16:41 +0700

    registrasi berhasil

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,7 +7,7 @@ var config = require('../config/secret');
 var ip = require('ip');
 
 // controller untuk register
-exports.registrasi = function(req, res){
+exports.registrasi = function (req, res) {
     var post = {
         username: req.body.username,
         email: req.body.email,
@@ -16,28 +16,28 @@ exports.registrasi = function(req, res){
         tanggal_daftar: new Date()
     }
 
-    var query = "SELECT email FROM ?? WHERE ??";
+    var query = "SELECT email FROM ?? WHERE ??=?";
     var table = ["user", "email", post.email];
 
-    query = mysql.format(query,table);
+    query = mysql.format(query, table);
 
-    connection.query(query, function(error, rows){
-        if(error){
+    connection.query(query, function (error, rows) {
+        if (error) {
             console.log(error);
-        }else{
-            if(rows.length == 0){
+        } else {
+            if (rows.length == 0) {
                 var query = "INSERT INTO ?? SET ?";
                 var table = ["user"];
                 query = mysql.format(query, table);
-                connection.query(query, post, function(error, rows){
-                    if(error){
+                connection.query(query, post, function (error, rows) {
+                    if (error) {
                         console.log(error);
-                    }else {
+                    } else {
                         response.ok("Berhasil menambahkan data user baru", res);
                     }
                 });
             } else {
-                response.ok("Email sudah terdaftar!")
+                response.ok("Email sudah terdaftar!", res);
             }
         }
     });
